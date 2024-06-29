60cadd2b70a974a77456fd61f9d279f613407cb8
tapan <tapanhegde@gmail.com>
2024-06-29 19:47:11 +0530

    first commit

--- src/main/java/com/learn/bookstoremanagement/model/Book.java
+++ src/main/java/com/learn/bookstoremanagement/model/Book.java
@@ -1,4 +1,65 @@
 package com.learn.bookstoremanagement.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.security.PublicKey;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String author;
+    private String price;
+    private String category;
+    private Integer stock;
+
+    // Getter and Setter
+    public Book(){
+    }
+
+    public Book(String title, String author, String price, String category,Integer stock){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.category = category;
+        this.stock = stock;
+    }
+    public Long getId() {return id;}
+
+    public void setId() {this.id = id;}
+
+    public String getTitle() { return title;}
+
+    public void setTitle() {
+        this.title = title;
+    }
+
+    public String getPrice() {return price;}
+
+    public void setPrice(){
+        this.price = price;
+    }
+
+    public String getCategory(){ return category; }
+
+    public void setCategory(){
+        this.category = category;
+    }
+
+    public String getAuthor(){ return author;}
+
+    public void setAuthor(){
+        this.author = author;
+    }
+
+    public Integer getStock() { return stock;}
+
+    public void setStock(){
+        this.stock = stock;
+    }
 }
--- src/main/java/com/learn/bookstoremanagement/model/OrderItem.java
+++ src/main/java/com/learn/bookstoremanagement/model/OrderItem.java
@@ -1,4 +1,85 @@
 package com.learn.bookstoremanagement.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "order_items")
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    private Integer quantity;
+
+    private Double price;
+
+    public OrderItem()
+    {
+
+    }
+
+    public OrderItem(Order order,Book book, Integer quantity, Double price){
+        this.order = order;
+        this.book = book;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    //Getters and Setters
+    public Long getId(){ return id;}
+
+    public void setId(){ this.id = id;}
+
+    public Order getOrder(){
+        return order;
+    }
+
+    public void setOrder(){
+        this.order = order;
+    }
+
+    public Book getBook(){
+        return book;
+    }
+
+    public void setBook(){
+        this.book = book;
+    }
+
+    public Integer getQuantity(){
+        return quantity;
+    }
+
+    public void setQuantity(){
+        this.quantity = quantity;
+    }
+
+    public Double getPrice(){
+        return price;
+    }
+
+    public void setPrice(){
+        this.price = price;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "OrderItem{" +
+                "id=" + id +
+                ", order=" + order +
+                ", book=" + book +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                '}';
+    }
 }
